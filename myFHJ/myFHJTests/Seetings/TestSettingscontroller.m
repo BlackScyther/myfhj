ec2357fc9870adbb7e932a7e64bda2039fdec2ed
ma.scheucher@gmail.com <ma.scheucher@gmail.com@3c5e0012-96a7-2652-f715-c08885339454>
2011-07-16 12:55:57 +0000

    tests removed

--- myFHJ/myFHJTests/Seetings/TestSettingscontroller.m
+++ myFHJ/myFHJTests/Seetings/TestSettingscontroller.m
@@ -14,28 +14,11 @@
 - (void) setUp 
 {
     settingsController = [[SettingsController alloc] init];
-    firstName = [[NSUserDefaults standardUserDefaults] stringForKey:@"userFirstname"];
-    lastName = [[NSUserDefaults standardUserDefaults] stringForKey:@"userLastname"];
 }
 
 - (void) tearDown
 {
     [settingsController release];
-    NSUserDefaults *defaults =[NSUserDefaults standardUserDefaults];
-    [defaults setObject:firstName forKey:@"userFirstname"];
-    [defaults setObject:lastName forKey:@"userLastname"];
-}
-
-
-- (void) testSaveText 
-{
-    UITextField *txt = [[UITextField alloc] init];
-    txt.text = @"test";
-    txt.tag = 1;
-    
-    [settingsController saveText:txt];
- 
-    STAssertTrue([[[NSUserDefaults standardUserDefaults] stringForKey:@"userFirstname"] isEqualToString:@"test"], @"");
 }
 
 @end
