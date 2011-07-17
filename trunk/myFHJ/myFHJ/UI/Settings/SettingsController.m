d3d9bfeaece9fd0b9b9180c79c5a716fcf88cbc2
ma.scheucher@gmail.com <ma.scheucher@gmail.com@3c5e0012-96a7-2652-f715-c08885339454>
2011-07-17 09:48:51 +0000

    update

--- trunk/myFHJ/myFHJ/UI/Settings/SettingsController.m
+++ trunk/myFHJ/myFHJ/UI/Settings/SettingsController.m
@@ -180,7 +180,7 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 140;
 
 - (IBAction) setYear:(id)sender 
 {
-    NSArray *years = [NSArray arrayWithObjects:@"2008", @"2007", @"2006", @"2005", @"2004", nil];
+    NSArray *years = [NSArray arrayWithObjects:@"2010", @"2009", @"2008", @"2007", nil];
     
     [self showActionSheetWithTitle:NSLocalizedString(@"settings.year", @"Year") buttons:years sheet:3];
 }
